9c65b57907928e5d826e405f3a815b8d9f5d263c
huizai1 <237002893@qq.com>
2022-12-04 20:42:54 +0000

    page users edit

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -2,7 +2,7 @@ import wepy from '@wepy/core'
 import { login, logout, refresh, register } from '@/api/auth'
 import * as auth from '@/utils/auth'
 import isEmpty from 'lodash/isEmpty'
-import { getCurrentUser } from '@/api/user'
+import { getCurrentUser, updateUser } from '@/api/user'
 
 const getDefaultState = () => {
   return {
@@ -63,6 +63,13 @@ const actions = {
     await register(params)
 
     await dispatch('login')
+  },
+  async updateUser ({ commit }, params = {}) {
+
+    const editResponse = await updateUser(params)
+
+    commit('setUser', editResponse.data)
+    auth.setUser(editResponse.data)
   }
 }
 
