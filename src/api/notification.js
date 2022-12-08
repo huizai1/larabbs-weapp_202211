7fb7a0a9a8de950c4954ae03c483a090df2d039f
huizai1 <237002893@qq.com>
2022-12-08 18:32:35 +0000

    page notification index

--- src/api/notification.js
+++ src/api/notification.js
@@ -3,3 +3,15 @@ import { authRequest } from '@/utils/request'
 export function getNotificationStats(...params) {
   return authRequest('notifications/stats', ...params)
 }
+
+export function getNotifications(data) {
+  return authRequest('notifications', {
+    data: data
+  })
+}
+
+export function readNotifications() {
+  return authRequest('user/read/notifications', {
+    method: 'PUT'
+  })
+}
